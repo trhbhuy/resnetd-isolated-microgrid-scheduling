bc7eaf7f4e73bda7856fd3c66ddaf480072c399a
trhbhuy <trhbhuy@sch.ac.kr>
2024-09-08 14:52:40 +0900

    update flexbile_load.py

--- src/solver/platform/components/load.py
+++ src/solver/platform/components/load.py
@@ -2,14 +2,18 @@ import gurobipy as gp
 from gurobipy import GRB
 
 class FlexibleLoad:
-    def __init__(self, ls_setting, phi_ls, T_set):
-        self.ls_setting = ls_setting
-        self.phi_ls = phi_ls
+    def __init__(self, T_set, ls_setting, phi_ls):
+        """Initialize parameters."""
         self.T_set = T_set
 
-    def add_variables(self, model, p_fl):
-        p_ls = model.addVars(self.T_set, lb=self.ls_setting*p_fl, ub=p_fl, vtype=GRB.CONTINUOUS, name="p_ls_1")
+        self.ls_setting = ls_setting
+        self.phi_ls = phi_ls
+
+    def add_variables(self, model, p_fl, var_name='p_ls'):
+        """Add variables to the model."""
+        p_ls = model.addVars(self.T_set, lb=self.ls_setting*p_fl, ub=p_fl, vtype=GRB.CONTINUOUS, name=var_name)
         return p_ls
     
     def get_cost(self, p_fl, p_ls):
+        """Calculate the DR cost of flexible load."""
         return gp.quicksum(((p_fl[t] - p_ls[t]) * self.phi_ls) for t in self.T_set)
